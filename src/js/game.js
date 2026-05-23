a312e33edf7789e6330229f00d64d734147c2d50
oiwiio <6gish6@gmail.com>
2026-05-23 16:16:01 +0500

    v1.5.0 рекорды сброшены

--- src/js/game.js
+++ src/js/game.js
@@ -546,12 +546,6 @@
             ctx.fillText(`лучший полёт: ${highScore} ✦`, canvas.width / 2 - 80, canvas.height - 60);
         }
 
-        if (!localStorage.getItem('sunrise_reset_done')) {
-        localStorage.removeItem('sunrise_lightness');
-        localStorage.setItem('sunrise_reset_done', 'true');
-        highScore = 0;
-        console.log('🏆 Рекорды сброшены (однократно)');
-    }
         
         // парящие частицы
         for (let i = 0; i < 12; i++) {
